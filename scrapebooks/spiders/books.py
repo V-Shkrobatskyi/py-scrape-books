59b848f820bd373bd4d66bac5587f035178dc4ec
Vitalii <vetali5700gmail.com>
2025-09-29 23:58:12 +0300

    add books spider

---
+++ scrapebooks/spiders/books.py
@@ -0,0 +1,39 @@
+import scrapy
+from scrapy.http import Response
+
+
+class BooksSpider(scrapy.Spider):
+    name = "books"
+    allowed_domains = ["books.toscrape.com"]
+    start_urls = ["https://books.toscrape.com/"]
+    RATING = {
+        "One": 1,
+        "Two": 2,
+        "Three": 3,
+        "Four": 4,
+        "Five": 5,
+    }
+
+    def parse(self, response: Response):
+        for book in response.css("h3 a::attr(href)").getall():
+            yield response.follow(book, callback=self.parse_book)
+
+        next_page = response.css("li.next a::attr(href)").get()
+        if next_page:
+            yield response.follow(next_page, callback=self.parse)
+
+    def parse_book(self, response: Response):
+        yield {
+            "title": response.css("div.product_main h1::text").get(default=""),
+            "price": response.css("p.price_color::text").get(default=""),
+            "amount_in_stock": "".join(
+                [s for s in response.css("p.instock::text").getall()[1] if s.isdigit()]
+            ),
+            "rating": self.RATING.get(
+                response.css("p.star-rating::attr(class)").get().split()[-1],
+                None
+            ),
+            "category": response.css("ul.breadcrumb li a::text").getall()[-1],
+            "upc": response.css("table.table.table-striped tr td::text").get(default=""),
+            "description": response.css("#product_description ~ p::text").get(default=""),
+        }
